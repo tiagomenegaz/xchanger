1b0f5890acff444df7368bc16bff1076ac652f36
Tiago Garcia <tiago.tiago22@gmail.com>
2017-05-04 21:47:14 -0300

    [FRONTEND] adds main layout

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Exchange Currency Process", :type => :feature do
+  it "exchange value" do
+    visit '/'
+    within("#exchange_form") do
+      select('EUR', from: 'currency')
+      select('USD', from: 'currency_destination')
+      fill_in 'quantity', with: '10'
+    end
+    click_button 'Submit'
+
+    expect(page).to have_content("value")
+  end
+end
